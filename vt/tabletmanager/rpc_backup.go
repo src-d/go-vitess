vitessio/vitess

Add RestoreFromBackup command to vtctl.
Executing the command vttablet will completely delete the existing data and
then replace it from the latest backup. The sequence of actions is basically
the same as vttablet has already used to setup a freshly started replica.

Also mysqlctl interface is extended with ReinitConfig function. It's used by
vttablet to change server_id of the replica before restoring it from backup.
That's necessary to avoid a possible situation when restored replica skips
transactions in the replication stream which have the same server_id.

The new RestoreFromBackup command will be used later in the implementation of
an automated pivot.

This change doesn't add any tests for the new functionality because I didn't
find a place where I could add these tests. Please tell me if there is actually
a place that I missed. Meanwhile I tested this manually on the example
Kubernetes setup.

go/vt/tabletmanager/rpc_backup.go
@@ -67,3 +67,26 @@ func (agent *ActionAgent) Backup(ctx context.Context, concurrency int, logger lo
 
 	return returnErr
 }
+
+// RestoreFromBackup deletes all local data and restores anew from the latest backup.
+// Should be called under RPCWrapLockAction.
+func (agent *ActionAgent) RestoreFromBackup(ctx context.Context, logger logutil.Logger) error {
+	tablet, err := agent.TopoServer.GetTablet(ctx, agent.TabletAlias)
+	if err != nil {
+		return err
+	}
+	if tablet.Type == topodatapb.TabletType_MASTER {
+		return fmt.Errorf("type MASTER cannot restore from backup, if you really need to do this, restart vttablet in replica mode")
+	}
+
+	// create the loggers: tee to console and source
+	l := logutil.NewTeeLogger(logutil.NewConsoleLogger(), logger)
+
+	// now we can run restore
+	err = agent.RestoreData(ctx, l, true /* deleteBeforeRestore */)
+
+	// re-run health check to be sure to capture any replication delay
+	agent.runHealthCheckProtected()
+
+	return err
+}

src-d/go-vitess

vt/tabletmanager/rpc_backup.go
@@ -67,3 +67,26 @@ func (agent *ActionAgent) Backup(ctx context.Context, concurrency int, logger lo
 
 	return returnErr
 }
+
+// RestoreFromBackup deletes all local data and restores anew from the latest backup.
+// Should be called under RPCWrapLockAction.
+func (agent *ActionAgent) RestoreFromBackup(ctx context.Context, logger logutil.Logger) error {
+	tablet, err := agent.TopoServer.GetTablet(ctx, agent.TabletAlias)
+	if err != nil {
+		return err
+	}
+	if tablet.Type == topodatapb.TabletType_MASTER {
+		return fmt.Errorf("type MASTER cannot restore from backup, if you really need to do this, restart vttablet in replica mode")
+	}
+
+	// create the loggers: tee to console and source
+	l := logutil.NewTeeLogger(logutil.NewConsoleLogger(), logger)
+
+	// now we can run restore
+	err = agent.restoreDataLocked(ctx, l, true /* deleteBeforeRestore */)
+
+	// re-run health check to be sure to capture any replication delay
+	agent.runHealthCheckProtected()
+
+	return err
+}